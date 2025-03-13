f77be1e681974b0b04811366f96a41a2b81d61b1
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-03-13 11:07:14 +0100

    tränat på javascript efefefefaergaåwoeigjaåweiruhgaåiewurhgiåuaewrhgåiuaerhgåiuaher

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,14 @@
+// var ålder = 1;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+// alert(tal1 + "+" + tal2 + "=" + summa);
+
+var förnamn = "Alexander";
+var efternamn = "Bolander";
+var helaNamnet = förnamn + "" + efternamn;
+var hobby = "lucky star";
+var kostnad = 400;
+
+var mening = helaNamnet + " " + "älskar" + " " + hobby + "\nMin hobby kostar" + " " + kostnad + " kr en gång "; 
+alert(mening);
