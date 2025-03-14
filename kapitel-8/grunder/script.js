2d40631783e5ccb17798bb6db67c46c2df72dddb
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-03-14 14:03:06 +0100

    tränat på java och grindit pokerouge

---
+++ kapitel-8/grunder/script.js
@@ -0,0 +1,39 @@
+// var dag = "fredag";
+// alert(dag);
+// console.log("idag är det " + dag);
+
+// var stad = prompt("var är du född");
+// console.log("Du är född i " + stad );
+
+// var gissning = prompt("är det okej att fuska på ett prov");
+// if (gissning == "ja") {
+//     alert("du är en människa");
+// } else {
+//     alert ("du är en robot")
+// }
+
+// var slump1 = Math.round(Math.random() * 100);
+// var slump2 = Math.round(Math.random() * 100);
+
+// var gooss = prompt("vad är " + slump1 + " + " + slump2 + "?");
+// if (condition) {
+//     document.writeln("<h1> DU är en människa<h1>");
+// } else {
+//     document.writeln("<h1>du är en robot<h1>");
+// }
+
+// var tal1 = prompt ("skriv ett nummer");
+// var tal2 = prompt ("skriv ett nummer");
+// var summa = Number("Summan är" + summa);
+// document.writeln("summan är " + summa);
+
+var inkoffffffffffmst = prompt ("hur mycket du får");
+var skattt = prompt ("vilken skattttgt");
+var sktttttttg = Number(skattt/100)
+var skarrrfg = Number(inkoffffffffffmst)  * Number(sktttttttg);
+alert(skarrrfg);
+
+
+
+
+//document.writeln("<h1>slump1 = " + slump1 + "<h1>");
