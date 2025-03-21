69758ac38a9accf376cc01a9d66743931e40d8fe
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-03-21 14:06:15 +0100

    commit

---
+++ kapitel-8/events/script.js
@@ -0,0 +1,19 @@
+// while (true) {
+//     let namn = prompt("ange ditt namn");
+//     let pass = prompt("ange ditt lösenord");
+
+//     if (namn == "alex" && pass == "12345" ) {
+//         document.writeln("Du är inloggad");  
+//         break;    
+//     } else {
+//         document.writeln("kys");
+//     }
+// }
+// let ald = prompt("hur många år är du");
+// document.writeln(`Hej ${namn} du är ${ald}`);
+let hel = parseInt(prompt("skriv ett heltaö mmemeel 5 - 10222222222234987"));
+for (leti = 1; i <= hel; i++) {
+    document.writeln(`raf ${i}<br>`)
+    
+}
+
