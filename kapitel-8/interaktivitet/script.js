cc5bc414e3b71e669ab7d0643127d55bb7931184
Blexander Bolander <alexander.bolander@elev.ga.ntig.se>
2025-03-27 11:08:30 +0100

    sigma commit

---
+++ kapitel-8/interaktivitet/script.js
@@ -0,0 +1,18 @@
+function text() {
+    document.body.style = "background: #000; color: #FFF;";
+    document.querySelector("h1").style = "font-size: 100px"
+}
+function sex() {
+    
+    document.body.style = "background: #FFF; color: #000;";
+}
+
+function sexrubrik() {
+    document.querySelector("h1").style = "font-size: 100px;text-transform:lowercase;text-shadow: 5px 5px 5px yellowgreen;background: khaki;";
+    document.body.style = "background: #FFF; color: #000;";
+}
+
+function sxe() {
+    document.querySelector("p").style = "visibility: visible";
+}
+
